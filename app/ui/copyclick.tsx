92aa5aa9b4f1f7a5fd3655b196600cfbcf6ff100
kv_ <138361348+kvirulent@users.noreply.github.com>
2024-12-20 19:29:13 -0600

    super duper update i forgot to track these files

---
+++ app/ui/copyclick.tsx
@@ -0,0 +1,26 @@
+// Adds the provided item to the clipboard when the element is clicked. Only tested with text as the content and copied object.
+"use client";
+import { useState } from "react";
+
+interface CopyClickProps {
+    displayText: string,
+    copyText: string
+}
+
+const CopyClick = ({displayText, copyText}: CopyClickProps) => {
+    const [innerText, setInnerText] = useState(displayText);
+
+    function copy() {
+        navigator.clipboard.writeText(copyText);
+        setInnerText("copied!");
+        setTimeout(() => {
+            setInnerText(displayText);
+        }, 650)
+    }
+
+    return (
+        <div className="cursor-pointer" onClick={copy}>{innerText}</div>
+    )
+}
+
+export default CopyClick;
